24006eadbc2afe1edc180baf14d39cd6450c2029
Oliver Thomsett <o.thomsett@gmail.com>
2014-02-07 18:07:31 +1100

    update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :get_contact_info
+  before_action :authenticate_user!
 
   def get_contact_info
   	 @contact_info = ContactInfo.first
